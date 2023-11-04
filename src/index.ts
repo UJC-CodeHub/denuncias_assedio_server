3092f1fc018c5a53b75a0733c12eb071b905017c
Melvin Nunes <melvinnunes14@gmail.com>
2023-11-04 11:44:23 +0200

    feat: added environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const port = process.env.PORT || 8000;
+const port = process.env.SERVER_PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
