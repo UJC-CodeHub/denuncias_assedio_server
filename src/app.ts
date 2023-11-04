080d37f8af8cf76d519e0fa0f4b7a299c90fa66b
Melvin Nunes <melvinnunes14@gmail.com>
2023-11-04 11:46:54 +0200

    feat: added health api

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api/health", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Hello World UJC",
+    message: "API is health!",
   });
 });
 
